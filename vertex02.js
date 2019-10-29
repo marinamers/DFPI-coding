394f938cb6f90aa97b6d6ed512722266d1816c75
marinamers <55982332+marinamers@users.noreply.github.com>
2019-10-29 02:23:48 +0000

    vertex with rotation

--- vertex02.js
+++ vertex02.js
@@ -12,20 +12,36 @@ uniform mat4 uViewMatrix;
 
 // setup the uniform for time
 uniform float uTime;
+
 // setup the uniform for translate
 uniform vec3 uTranslate;
-uniform vec3 uScale;
 
-uniform vec3 uColors;
+uniform vec3 uScale;
+uniform vec3 uRotate;
+
+uniform vec4 uColors;
 
 // setup varying to pass the uv to the fragment
 varying vec2 vUV;
 
+//rotate
+vec2 rotate(vec2 v, float a) {
+	float s = sin(a);
+	float c = cos(a);
+	mat2 m = mat2(c, -s, s, c);
+	return m * v;
+}
+
 void main() {
   vec3 pos = aPosition;
+  float angle = cos(uTranslate.x + uTranslate.y + uTime);
+  //pos.xy = rotate(pos.xy, uRotate.x);// for tate experimet1
+  pos.xy = rotate(pos.xy,angle);
+  //uRotate.xyz *= 0.3;
+
 
   // add the translate to the position
-  pos.yxz*=0.5 * uScale.x;
+  pos.yxz*=0.3 * uScale.x;
   pos += uTranslate;
 
   gl_Position = uProjectionMatrix * uViewMatrix * vec4(pos, 1.0);
